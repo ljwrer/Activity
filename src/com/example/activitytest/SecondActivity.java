927c88b2c6ef341c48631802fe74f9a481b9f5ce
ljwrer <ljwrer@sina.com>
2015-04-02 15:11:58 +0800

    ljw

---
+++ src/com/example/activitytest/SecondActivity.java
@@ -0,0 +1,21 @@
+package com.example.activitytest;
+
+import com.example.activitytest.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+
+public class SecondActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.second_layout);
+		Intent intent=getIntent();
+		String data=intent.getStringExtra("extra_data");
+		Log.d("secondActivity",data);
+	}
+}
