0c00902e14ab5beb0aa3ac28de94285fe7718d4b
ljwrer <ljwrer@sina.com>
2015-04-03 17:30:23 +0800

    genneral

--- src/com/example/activitytest/FirstActivity.java
+++ src/com/example/activitytest/FirstActivity.java
@@ -14,22 +14,28 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class FirstActivity extends Activity {
+public class FirstActivity extends BaseActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+//		Log.d("FirstActivity", this.toString());
+		Log.d("FirstActivity","Task id is"+getTaskId());
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.first_layout);
 		Button button1=(Button)findViewById(R.id.button_1);
 		button1.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				String data="hello secondactivity";
-				
+//				String data="hello secondactivity";
+				SecondActivity.actionStart(FirstActivity.this, "data1", "data2");
 				//				Toast.makeText(FirstActivity.this, "You clicked Button 1", Toast.LENGTH_SHORT).show();
 //				finish();
-				Intent intent=new Intent(FirstActivity.this,SecondActivity.class);
-				startActivityForResult(intent, 1);
+//				Intent intent=new Intent(FirstActivity.this,SecondActivity.class);
+//				Bundle mbundle=new Bundle();
+//				String returnData="Return Hello FirstActivity";
+//				mbundle.putString("return_Data", returnData);
+//				intent.putExtras(mbundle);
+//				startActivityForResult(intent, 1);
 //				intent.putExtra("extra_data", data);
 //				Intent intent=new Intent("com.example.activitytest.ACTION_START");
 //				intent.addCategory("com.example.activitytest.MY_CATEGORY");
@@ -39,17 +45,17 @@ public class FirstActivity extends Activity {
 			}
 		});
 	}
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		// TODO Auto-generated method stub
-//		Log.d("request11", requestCode+" "+resultCode);
-//		super.onActivityResult(requestCode, resultCode, data);
-		if(resultCode==RESULT_OK)
-		{
-			String returnedData=data.getStringExtra("data_return");
-			Log.d("FirstActivity",returnedData);
-		}
-	}
+//	@Override
+//	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//		// TODO Auto-generated method stub
+////		Log.d("request11", requestCode+" "+resultCode);
+////		super.onActivityResult(requestCode, resultCode, data);
+//		if(resultCode==RESULT_OK)
+//		{
+//			String returnedData=data.getStringExtra("data_return");
+//			Log.d("FirstActivity",returnedData);
+//		}
+//	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.main, menu);
@@ -67,4 +73,10 @@ public class FirstActivity extends Activity {
 		}
 		return true;
 	}	
+	@Override
+	protected void onRestart() {
+		// TODO Auto-generated method stub
+		super.onRestart();
+		Log.d("FirstActivity", "onRestart");
+	}
 }
--- src/com/example/activitytest/SecondActivity.java
+++ src/com/example/activitytest/SecondActivity.java
@@ -2,7 +2,9 @@ package com.example.activitytest;
 
 import com.example.activitytest.R;
 
+import android.R.string;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,24 +13,28 @@ import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.Button;
 
-public class SecondActivity extends Activity {
+public class SecondActivity extends BaseActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Log.d("secondActivity","Task id is"+getTaskId());
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.second_layout);
+//		String data = savedInstanceState.getString("return_Data");
+//		Log.d("secondActivity",data);
 //		Intent intent=getIntent();
-//		String data=intent.getStringExtra("extra_data");
+//		String data=intent.getStringExtra("return_Data");
 //		Log.d("secondActivity",data);
 		Button button2=(Button)findViewById(R.id.button_2);
 		button2.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				Intent intent=new Intent();
-				intent.putExtra("data_return", "Hello FirstActivity");
-				setResult(RESULT_OK,intent);
-				finish();
+				Intent intent=new Intent(SecondActivity.this,ThirdActivity.class);
+				startActivity(intent);				
+//				intent.putExtra("data_return", "Hello FirstActivity");
+//				setResult(RESULT_OK,intent);
+//				finish();
 			}
 		});
 	}
@@ -39,4 +45,17 @@ public class SecondActivity extends Activity {
 		setResult(RESULT_OK,intent);
 		finish();
 }
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		Log.d("secondActivity","onDestriy");
+	}
+	public static void actionStart(Context context,String data1,String data2) {
+		Intent intent=new Intent(context,SecondActivity.class);
+		intent.putExtra("param1", data1);
+		intent.putExtra("param2", data2);
+		context.startActivity(intent);
+	}
+
 }
